8fa0fafbe8c31e60a8ba285fbb4cb997b5528291
abdullahsuhail062 <abdullahsuhail062@gmail.com>
2025-04-13 01:04:17 +0500

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component, ViewChild } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MatSlideToggle, MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,MatSlideToggleModule],
+  imports: [RouterOutlet,MatSlideToggleModule,NgIf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
