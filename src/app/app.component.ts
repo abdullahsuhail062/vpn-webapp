4656716e9cdec9f659b93754cc27771353eb01ea
abdullahsuhail062 <abdullahsuhail062@gmail.com>
2025-04-12 23:43:53 +0500

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatSlideToggle, MatSlideToggleModule } from '@angular/material/slide-toggle';
+
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet,MatSlideToggleModule],
@@ -9,4 +10,16 @@ import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 })
 export class AppComponent {
   title = 'multipleLoginOptionsIntegration-webapp';
+    @ViewChild('toggle') toggle!: MatSlideToggle;
+
+  getFocus(){
+    const isToggled = this.toggle.checked
+    if (!isToggled) {
+      alert('Please toggle button before submitting')
+      this.toggle.focus()
+      
+    }else alert('form submitted!')
+  }
+
+
 }
