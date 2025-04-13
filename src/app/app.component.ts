1f8a6f74011dc5013a734e1f2d5847f451f8c13e
abdullahsuhail062 <abdullahsuhail062@gmail.com>
2025-04-13 19:04:03 +0500

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,11 +8,12 @@ import { MatSelectModule } from '@angular/material/select';
 import { MatInputModule } from '@angular/material/input'; // if using <input> inside
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'; // r
 import { Router } from '@angular/router';
+import { CountryListComponent } from './country-list/country-list.component';
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,MatSlideToggleModule,NgIf,FormsModule,MatFormFieldModule,MatSelectModule,MatInputModule,],
+  imports: [RouterOutlet,MatSlideToggleModule,NgIf,FormsModule,MatFormFieldModule,MatSelectModule,MatInputModule,CountryListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
