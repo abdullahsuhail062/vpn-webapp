73691a77ca211d413a7d03df92b154867c4a19f6
abdullahsuhail062 <abdullahsuhail062@gmail.com>
2025-04-13 22:45:08 +0500

    update

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,5 +4,5 @@ import { CountryListComponent } from './country-list/country-list.component';
 import { AppComponent } from './app.component';
 
 export const routes: Routes = [{path: '', component: AppComponent, children:[{
-    path: 'country-list', component: CountryListComponent}]}
+    path: 'country-list', title: 'country list', component: CountryListComponent}]}
 ];
