33a3166f6a81da5add1add40e415d0b9e757e85b
abdullahsuhail062 <abdullahsuhail062@gmail.com>
2025-04-13 23:12:15 +0500

    update

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,5 @@
 import { Routes } from '@angular/router';
 import { CountryListComponent } from './country-list/country-list.component';
 
-export const routes: Routes = [{path: '',  component: CountryListComponent}
+export const routes: Routes = [{children:[{path: 'country-list',  component: CountryListComponent}]}
 ];
