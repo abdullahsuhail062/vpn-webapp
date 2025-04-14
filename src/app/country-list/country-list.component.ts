ae809e03f083bfade2d0fdd2ed6c036905376639
abdullahsuhail062 <abdullahsuhail062@gmail.com>
2025-04-15 03:04:47 +0500

    update

--- src/app/country-list/country-list.component.ts
+++ src/app/country-list/country-list.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { MatListModule } from '@angular/material/list';
+import {MatCheckboxModule} from '@angular/material/checkbox';
 
 
 @Component({
   selector: 'app-country-list',
-  imports: [RouterLink,MatListModule],
+  imports: [RouterLink,MatListModule,MatCheckboxModule],
   templateUrl: './country-list.component.html',
   styleUrl: './country-list.component.scss'
 })
