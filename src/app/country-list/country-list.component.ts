1247142a7f00b0a37eb0154730593cfa232b8e2b
abdullahsuhail062 <abdullahsuhail062@gmail.com>
2025-04-14 12:54:55 +0500

    update

--- src/app/country-list/country-list.component.ts
+++ src/app/country-list/country-list.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
+import { MatListModule } from '@angular/material/list';
+
 
 @Component({
   selector: 'app-country-list',
-  imports: [RouterLink],
+  imports: [RouterLink,MatListModule],
   templateUrl: './country-list.component.html',
   styleUrl: './country-list.component.scss'
 })
 export class CountryListComponent {
+
+  countries = [
+    { name: 'United States', code: 'US', flag: '🇺🇸' },
+    { name: 'United Kingdom', code: 'UK', flag: '🇬🇧' },
+    { name: 'Germany', code: 'DE', flag: '🇩🇪' },
+    { name: 'India', code: 'IN', flag: '🇮🇳' },
+    { name: 'Australia', code: 'AU', flag: '🇦🇺' }
+  ];
+  
 
 }
