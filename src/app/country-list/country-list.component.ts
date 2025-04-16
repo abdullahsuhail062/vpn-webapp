2eb5cc47eb678501b4f67aac8567cf5cc2164acd
abdullahsuhail062 <abdullahsuhail062@gmail.com>
2025-04-16 12:38:55 +0500

    update

--- src/app/country-list/country-list.component.ts
+++ src/app/country-list/country-list.component.ts
@@ -2,19 +2,22 @@ import { Component, OnInit } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { MatListModule } from '@angular/material/list';
 import {MatCheckboxModule} from '@angular/material/checkbox';
+import { FormsModule, NgModel } from '@angular/forms';
 
 
 @Component({
   selector: 'app-country-list',
-  imports: [RouterLink,MatListModule,MatCheckboxModule],
+  imports: [RouterLink,MatListModule,MatCheckboxModule,FormsModule ],
   templateUrl: './country-list.component.html',
   styleUrl: './country-list.component.scss'
 })
 export class CountryListComponent implements OnInit {
+  isChecked:boolean = false
 
   ngOnInit(): void {
+    this.isChecked = true
     
-     this.update(true)   
+     this.update(this.isChecked)   
     
   }
 
