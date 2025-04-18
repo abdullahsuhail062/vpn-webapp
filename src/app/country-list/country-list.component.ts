b56da783aca1df72cf73a812600e53ac34a841dd
abdullahsuhail062 <abdullahsuhail062@gmail.com>
2025-04-18 20:50:14 +0500

    update

--- src/app/country-list/country-list.component.ts
+++ src/app/country-list/country-list.component.ts
@@ -12,11 +12,19 @@ import { FormsModule, NgModel } from '@angular/forms';
   templateUrl: './country-list.component.html',
   styleUrl: './country-list.component.scss'
 })
-export class CountryListComponent implements OnInit,AfterViewInit {
-  @ViewChild('unitedState')unitedState!: ElementRef;
+export class CountryListComponent implements OnInit {
+  @ViewChild('itemList')itemList!: ElementRef;
 
   isChecked:boolean = false
-  @Output() eventEmitter = new EventEmitter
+  @Output() itemClicked = new EventEmitter<{ image: string, text: string }>();
+  list = [
+    { image: 'images/unitedstate.png', text: 'United State' },
+    { image: 'images/unitedkingdom.png', text: 'United Kingdom' },
+    { image: 'images/germany.jpeg', text: 'Germany' },
+    {image:   'images/india.jpeg', text: 'India'},
+    {image: 'images/australia.jpeg', text: 'Australia'}
+  ];
+
 
   ngOnInit(): void {
     this.isChecked = true
@@ -32,20 +40,21 @@ export class CountryListComponent implements OnInit,AfterViewInit {
     }
 
 
-    ngAfterViewInit(): void {
-    this.connectToUnitedStateServer()
-    }
-    connectToUnitedStateServer(){
-
-      const divContent = this.unitedState.nativeElement
-
-      const text = this.unitedState.nativeElement.innerText;
-      const img: HTMLImageElement = divContent.querySelector('img');
-      console.log(img, text);
-      
+    onItemClick(event: MouseEvent): void {
+      const target = event.target as HTMLElement;
     
+      // Find the closest mat-list parent of the clicked item
+      const matListItem = target.closest('mat-list');
+      if (!matListItem) return;
+    
+      const img = matListItem.querySelector('img') as HTMLImageElement;
+      const text = matListItem.querySelector('span')?.textContent?.trim();
+    
+      console.log('Clicked item:');
+      console.log('Image src:', img?.src);
+      console.log('Alt text:', img?.alt);
+      console.log('Text:', text);
     }
-  
 
    }
 
