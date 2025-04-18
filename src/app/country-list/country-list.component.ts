ac36f9262d926dd680b3edef5cc7cd85a43b2814
abdullahsuhail062 <abdullahsuhail062@gmail.com>
2025-04-18 11:13:04 +0500

    update

--- src/app/country-list/country-list.component.ts
+++ src/app/country-list/country-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, EventEmitter, OnInit, Output, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, EventEmitter, OnInit, Output, ViewChild } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { MatListModule } from '@angular/material/list';
 import {MatCheckboxModule} from '@angular/material/checkbox';
@@ -12,7 +12,7 @@ import { FormsModule, NgModel } from '@angular/forms';
   templateUrl: './country-list.component.html',
   styleUrl: './country-list.component.scss'
 })
-export class CountryListComponent implements OnInit {
+export class CountryListComponent implements OnInit,AfterViewInit {
   @ViewChild('unitedState')unitedState!: ElementRef;
 
   isChecked:boolean = false
@@ -35,6 +35,11 @@ export class CountryListComponent implements OnInit {
     ngAfterViewInit(): void {
       const divContent = this.unitedState.nativeElement.innerText;
       console.log('Div content:', divContent);
+      const img: HTMLImageElement = divContent.querySelector('img');
+    if (img) {
+      console.log('Image src:', img.src);
+      console.log('Image alt:', img.alt);
+    }
     }
   
 
