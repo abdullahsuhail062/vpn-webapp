4781c281ce8d48c671df5679550e507156e7f2a0
abdullahsuhail062 <abdullahsuhail062@gmail.com>
2025-04-18 10:35:03 +0500

    update

--- src/app/country-list/country-list.component.ts
+++ src/app/country-list/country-list.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, EventEmitter, OnInit, Output, ViewChild } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { MatListModule } from '@angular/material/list';
 import {MatCheckboxModule} from '@angular/material/checkbox';
 import { FormsModule, NgModel } from '@angular/forms';
+
 
 
 @Component({
@@ -12,7 +13,10 @@ import { FormsModule, NgModel } from '@angular/forms';
   styleUrl: './country-list.component.scss'
 })
 export class CountryListComponent implements OnInit {
+  @ViewChild('ununitedState')unitedState!: ElementRef;
+
   isChecked:boolean = false
+  @Output() eventEmitter = new EventEmitter
 
   ngOnInit(): void {
     this.isChecked = true
@@ -25,6 +29,12 @@ export class CountryListComponent implements OnInit {
   update(event: boolean){
     event = true
        
+    }
+
+
+    ngAfterViewInit(): void {
+      const divContent = this.unitedState.nativeElement.innerText;
+      console.log('Div content:', this.unitedState);
     }
   
 
