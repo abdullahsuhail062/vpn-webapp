2500495841e9d69e5ae76d55279ec6e77c4b6f8b
abdullahsuhail062 <abdullahsuhail062@gmail.com>
2025-04-20 07:22:48 +0500

    update

--- src/app/country-list/country-list.component.ts
+++ src/app/country-list/country-list.component.ts
@@ -42,7 +42,9 @@ export class CountryListComponent implements OnInit {
 
 
     onUsaItemClick(): void {
-      this.itemClicked.emit({image:'images/unitedstate.png', text: 'United State'})
+     const testEvent = this.itemClicked.emit({image:'images/unitedstate.png', text: 'United State'})
+      console.log(testEvent);
+      
       this.router.navigate(['/'])
      
     }
